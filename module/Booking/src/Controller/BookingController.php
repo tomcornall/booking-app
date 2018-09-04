ab7d9f5c5484986f74a5ebbb32686ed260f77f82
tomcornall <tugatom@gmail.com>
2018-09-04 00:26:45 -0700

    Adds edit/delete methods to the Booking frontend Controller

--- module/Booking/src/Controller/BookingController.php
+++ module/Booking/src/Controller/BookingController.php
@@ -56,16 +56,75 @@ class BookingController extends AbstractActionController
     }
 
     /**
-     * Loads bookings edit view
+     * Loads bookings edit view, or edits a booking
      */
     public function editAction()
     {
+        $id = (int) $this->params()->fromRoute('id', 0);
+
+        if (0 === $id) {
+            return $this->redirect()->toRoute('booking', ['action' => 'add']);
+        }
+
+        // Retrieve the booking with the specified id. Doing so raises
+        // an exception if the booking is not found, which should result
+        // in redirecting to the landing page.
+        try {
+            $booking = $this->table->getBooking($id);
+        } catch (\Exception $e) {
+            return $this->redirect()->toRoute('booking', ['action' => 'index']);
+        }
+
+        $form = new BookingForm();
+        $form->bind($booking);
+        $form->get('submit')->setAttribute('value', 'Edit');
+
+        $request = $this->getRequest();
+        $viewData = ['id' => $id, 'form' => $form];
+
+        if (! $request->isPost()) {
+            return $viewData;
+        }
+
+        $form->setInputFilter($booking->getInputFilter());
+        $form->setData($request->getPost());
+
+        if (! $form->isValid()) {
+            return $viewData;
+        }
+
+        $this->table->saveBooking($booking);
+
+        // Redirect to booking list
+        return $this->redirect()->toRoute('booking', ['action' => 'index']);
     }
 
     /**
-     * Loads bookings delete view
+     * Delete bookings handler
      */
     public function deleteAction()
     {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('booking');
+        }
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $del = $request->getPost('del', 'No');
+
+            if ($del == 'Yes') {
+                $id = (int) $request->getPost('id');
+                $this->table->deleteBooking($id);
+            }
+
+            // Redirect to list of bookings
+            return $this->redirect()->toRoute('booking');
+        }
+
+        return [
+            'id'    => $id,
+            'booking' => $this->table->getBooking($id),
+        ];
     }
 }
