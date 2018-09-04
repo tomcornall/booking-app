c521af9fea9f17564d211bae12c734797ea70db2
tomcornall <tugatom@gmail.com>
2018-09-04 07:42:11 -0700

    Removes test config

--- module/Booking/config/module.config.php
+++ module/Booking/config/module.config.php
@@ -3,7 +3,6 @@
 namespace Booking;
 
 use Zend\Router\Http\Segment;
-// use Booking\Controller\BookingController;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
@@ -20,19 +19,6 @@ return [
                     'defaults' => [
                         'controller' => Controller\BookingController::class,
                         'action'     => 'index',
-                    ],
-                ],
-            ],
-            'booking-rest' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route'    => '/rest[/:action[/:id]]',
-                    'constraints' => [
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ],
-                    'defaults' => [
-                        'controller' => Controller\BookingRestController::class,
                     ],
                 ],
             ],
