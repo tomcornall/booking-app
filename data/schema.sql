a283069ddd03cf2df5c783cc0368be39cfa50fc7
tomcornall <tugatom@gmail.com>
2018-09-03 23:50:18 -0700

    Adds Schema for test db

---
+++ data/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE booking (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username VARCHAR(80) NOT NULL,
+  reason VARCHAR(255) NOT NULL,
+  start_date DATETIME NOT NULL,
+  end_date DATETIME NOT NULL
+);
+INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson', 'Back hurts', '2018-10-20T08:00:00+00:00', '2018-10-20T10:00:00+00:00');
+INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson 2', 'Back hurts 2', '2018-10-20T08:00:00+00:00', '2018-10-20T10:00:00+00:00');
+INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson 3rd', 'Back hurts 2', '2018-10-20T08:00:00+00:00', '2018-10-20T10:00:00+00:00');
