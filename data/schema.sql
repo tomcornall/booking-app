7bc302836719b89c4826982ef7de5035e10b3a2c
tomcornall <tugatom@gmail.com>
2018-09-04 07:41:29 -0700

    Better test names

--- data/schema.sql
+++ data/schema.sql
@@ -6,5 +6,5 @@ CREATE TABLE booking (
   end_date DATETIME NOT NULL
 );
 INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson', 'Back hurts', '2018-10-20T08:00', '2018-10-20T10:00');
-INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson 2', 'Back hurts 2', '2018-10-20T08:00', '2018-10-20T10:00');
-INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson 3rd', 'Back hurts 2', '2018-10-20T08:00', '2018-10-20T10:00');
+INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson the 2nd', 'Back hurts bad', '2018-10-20T08:00', '2018-10-20T10:00');
+INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson the 3rd', 'Back hurts real bad', '2018-10-20T08:00', '2018-10-20T10:00');
