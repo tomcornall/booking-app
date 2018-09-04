f1d7d406a986784ccec85e9c6ee97170c54414d5
tomcornall <tugatom@gmail.com>
2018-09-04 00:55:55 -0700

    Removes timezone from schema inserts

--- data/schema.sql
+++ data/schema.sql
@@ -5,6 +5,6 @@ CREATE TABLE booking (
   start_date DATETIME NOT NULL,
   end_date DATETIME NOT NULL
 );
-INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson', 'Back hurts', '2018-10-20T08:00:00+00:00', '2018-10-20T10:00:00+00:00');
-INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson 2', 'Back hurts 2', '2018-10-20T08:00:00+00:00', '2018-10-20T10:00:00+00:00');
-INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson 3rd', 'Back hurts 2', '2018-10-20T08:00:00+00:00', '2018-10-20T10:00:00+00:00');
+INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson', 'Back hurts', '2018-10-20T08:00:00', '2018-10-20T10:00:00');
+INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson 2', 'Back hurts 2', '2018-10-20T08:00:00', '2018-10-20T10:00:00');
+INSERT INTO booking (username, reason, start_date, end_date) VALUES ('Tom Johnson 3rd', 'Back hurts 2', '2018-10-20T08:00:00', '2018-10-20T10:00:00');
